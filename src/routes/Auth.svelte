50daa59bc210a3909062b6be328f6ab5bd9d31d9
Steven <stevensuncatalin369@gmail.com>
2021-10-29 01:21:43 +0300

    Now it updates everytime the auth route is called.

--- src/routes/Auth.svelte
+++ src/routes/Auth.svelte
@@ -1,5 +1,63 @@
 <script>
-  // document.domain = 'elmtree.io'
+  import axios from "axios"
+  import {apiURL} from '../apiURL'
+  import {onMount} from 'svelte'
+
+  // updating user data
+  var ls = JSON.parse(localStorage.getItem('userInfo'))
+  var userType = localStorage.getItem('userType')
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${ls.token}`,
+    },
+  }
+
+  const updateTeacher =  async() => {
+    const update = await axios.get(
+      `${apiURL}/api/teacher/update`,
+      config 
+    )
+
+    ls.homeroomGrade = update.data.homeroomGrade
+    ls.subjectList = update.data.subjectList
+    ls.token = update.data.token
+  }
+
+  const updateStudent = async() => {
+    const update = await axios.get(
+      `${apiURL}/api/student/update`,
+      config
+    )
+
+    ls.grade = update.data.grade
+    ls.subjectList = update.data.subjectList
+    ls.token = update.data.token
+  }
+
+  const updateParent = async() => {
+    const update = await axios.get(
+      `${apiURL}/api/parent/update`,
+      config
+    )
+
+    ls.students = updartedata.students,
+    ls.token = update.data.token
+  }
+  onMount(async () => {
+    if (userType === 'teacher') {
+      await updateTeacher()
+    } else if (userType === 'student') {
+      await updateStudent()
+    } else if (userType === 'parent') {
+      await updateParent()
+    }
+    localStorage.setItem('userInfo', JSON.stringify(ls))
+  })
+
+
+  // giving the auth info
   let data = {
       userType: localStorage.getItem("userType"),
       userInfo: localStorage.getItem("userInfo")
